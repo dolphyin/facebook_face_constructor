74e7a6e141c4d6f4b91fa4da81210a8b46a52698
briany <brianinyin@gmail.com>
2014-08-10 15:54:36 -0700

    initial commit

---
+++ facebook_face_constructor/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FacebookFaceConstructor::Application.config.secret_key_base = 'ccbf2c55291d41d0331b3a78dcc3fe1765c07f97a35e684979fefcf21e590741caa010278be1b7faed8ae3c866e5e8eedac3a657bdb297332116bd751dd2e3b8'
---
+++ facebook_face_constructor/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FacebookFaceConstructor::Application.config.session_store :cookie_store, key: '_facebook_face_constructor_session'
